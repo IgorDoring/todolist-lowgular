8e7b271a62cdf44f99960f92f91c967669cc93e3
IgorDoring <idoring@tutanota.com>
2024-09-06 08:01:21 -0300

    fix: removing unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
-import { AsyncPipe, CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BehaviorSubject, map, Observable, tap } from 'rxjs';
-import { Task, TaskResponse } from './model/task';
-import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
