9bee2c1c77ce60ee01cad57ffa81ce3df8477c99
IgorDoring <idoring@tutanota.com>
2024-09-06 08:23:04 -0300

    refactor: adjusting headers

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -10,45 +10,50 @@ import { TaskResponse } from '../../model/task';
   standalone: true,
   imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  private http: HttpClient = inject(HttpClient)
+  private http: HttpClient = inject(HttpClient);
   readonly projectId = '2335869742';
-  taskForm = {project_id: this.projectId, content: ''}
-  listSubject: BehaviorSubject<TaskResponse[]> = new BehaviorSubject<TaskResponse[]>([])
-  todolist$: Observable<TaskResponse[]> = this.listSubject.asObservable()
-  
+  readonly headers = {
+    headers: {
+      'Authorization': 'Bearer 4073e1ba35bd897d02b44a5ac75b019d0688be37',
+      'Content-Type': 'application/json',
+      'X-Request-Id': '2335869742',
+    },
+  };
+  taskForm = { project_id: this.projectId, content: '' };
+  listSubject: BehaviorSubject<TaskResponse[]> = new BehaviorSubject<
+    TaskResponse[]
+  >([]);
+  todolist$: Observable<TaskResponse[]> = this.listSubject.asObservable();
+
   ngOnInit(): void {
     this.loadTasks();
   }
 
-  addTask(taskForm: NgForm){
-    if(taskForm.valid){
-      this.http.post<TaskResponse>("https://api.todoist.com/rest/v2/tasks", JSON.stringify(this.taskForm), {
-      headers: {
-        "Authorization":"Bearer 4073e1ba35bd897d02b44a5ac75b019d0688be37",
-        "Content-Type": "application/json",
-        "X-Request-Id": "2335869742"
-      }
-    }).pipe(
-      tap( newTask => {
-        const currentTasks: TaskResponse[] = this.listSubject.getValue()
-        this.listSubject.next([...currentTasks, newTask])
-      })
-    ).subscribe()
+  addTask(taskForm: NgForm) {
+    if (taskForm.valid) {
+      this.http
+        .post<TaskResponse>(
+          'https://api.todoist.com/rest/v2/tasks',
+          JSON.stringify(this.taskForm),
+          this.headers
+        )
+        .pipe(
+          tap((newTask) => {
+            const currentTasks: TaskResponse[] = this.listSubject.getValue();
+            this.listSubject.next([...currentTasks, newTask]);
+          })
+        )
+        .subscribe();
+    }
   }
-}
 
-  loadTasks(){
-    this.http.get<TaskResponse[]>("https://api.todoist.com/rest/v2/tasks", {
-      headers: {
-        "Authorization":"Bearer 4073e1ba35bd897d02b44a5ac75b019d0688be37",
-        "Content-Type": "application/json",
-        "X-Request-Id": "2335869742"
-      }
-    }).pipe(
-      tap( tasks => this.listSubject.next(tasks) )
-    ).subscribe()
+  loadTasks() {
+    this.http
+      .get<TaskResponse[]>('https://api.todoist.com/rest/v2/tasks', this.headers)
+      .pipe(tap((tasks) => this.listSubject.next(tasks)))
+      .subscribe();
   }
 }
