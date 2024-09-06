1b1140493e79b009f33f99dc75af04b99932b9d6
IgorDoring <idoring@tutanota.com>
2024-09-06 08:00:20 -0300

    refactor: moving code from app component to new home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,48 +9,11 @@ import { FormsModule, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, CommonModule, HttpClientModule, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  private http: HttpClient = inject(HttpClient)
-  readonly projectId = '2335869742';
-  taskForm = {project_id: this.projectId, content: ''}
-  listSubject: BehaviorSubject<TaskResponse[]> = new BehaviorSubject<TaskResponse[]>([])
-  todolist$: Observable<TaskResponse[]> = this.listSubject.asObservable()
-  
-  ngOnInit(): void {
-    this.loadTasks();
-  }
+export class AppComponent {
 
-  addTask(taskForm: NgForm){
-    if(taskForm.valid){
-      this.http.post<TaskResponse>("https://api.todoist.com/rest/v2/tasks", JSON.stringify(this.taskForm), {
-      headers: {
-        "Authorization":"Bearer 4073e1ba35bd897d02b44a5ac75b019d0688be37",
-        "Content-Type": "application/json",
-        "X-Request-Id": "2335869742"
-      }
-    }).pipe(
-      tap( newTask => {
-        const currentTasks: TaskResponse[] = this.listSubject.getValue()
-        this.listSubject.next([...currentTasks, newTask])
-      })
-    ).subscribe()
-  }
-}
-
-  loadTasks(){
-    this.http.get<TaskResponse[]>("https://api.todoist.com/rest/v2/tasks", {
-      headers: {
-        "Authorization":"Bearer 4073e1ba35bd897d02b44a5ac75b019d0688be37",
-        "Content-Type": "application/json",
-        "X-Request-Id": "2335869742"
-      }
-    }).pipe(
-      tap( tasks => this.listSubject.next(tasks) )
-    ).subscribe()
-  }
 
 }
---
+++ src/app/core/home/home.component.ts
@@ -0,0 +1,54 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { TaskResponse } from '../../model/task';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  private http: HttpClient = inject(HttpClient)
+  readonly projectId = '2335869742';
+  taskForm = {project_id: this.projectId, content: ''}
+  listSubject: BehaviorSubject<TaskResponse[]> = new BehaviorSubject<TaskResponse[]>([])
+  todolist$: Observable<TaskResponse[]> = this.listSubject.asObservable()
+  
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  addTask(taskForm: NgForm){
+    if(taskForm.valid){
+      this.http.post<TaskResponse>("https://api.todoist.com/rest/v2/tasks", JSON.stringify(this.taskForm), {
+      headers: {
+        "Authorization":"Bearer 4073e1ba35bd897d02b44a5ac75b019d0688be37",
+        "Content-Type": "application/json",
+        "X-Request-Id": "2335869742"
+      }
+    }).pipe(
+      tap( newTask => {
+        const currentTasks: TaskResponse[] = this.listSubject.getValue()
+        this.listSubject.next([...currentTasks, newTask])
+      })
+    ).subscribe()
+  }
+}
+
+  loadTasks(){
+    this.http.get<TaskResponse[]>("https://api.todoist.com/rest/v2/tasks", {
+      headers: {
+        "Authorization":"Bearer 4073e1ba35bd897d02b44a5ac75b019d0688be37",
+        "Content-Type": "application/json",
+        "X-Request-Id": "2335869742"
+      }
+    }).pipe(
+      tap( tasks => this.listSubject.next(tasks) )
+    ).subscribe()
+  }
+}
