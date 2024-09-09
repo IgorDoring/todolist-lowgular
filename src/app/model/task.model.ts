1f9aad297a9950c510dd63a4ec27aad9b707bec4
IgorDoring <idoring@tutanota.com>
2024-09-09 17:28:14 -0300

    refactor: changing properties of task

--- src/app/model/task.model.ts
+++ src/app/model/task.model.ts
@@ -1,15 +1,11 @@
 export interface TaskResponse {
-  is_completed: boolean;
+  is_completed?: boolean;
   content: string;
   id: string;
-  order: number;
-  priority: number;
 }
 
 export interface TaskModel {
-  isCompleted: boolean;
+  isCompleted?: boolean;
   content: string;
   id: string;
-  order: number;
-  priority: number;
 }
